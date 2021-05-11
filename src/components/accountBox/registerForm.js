ab2089422aac8513ce628e6917c3fa6fa66f65a6
Mehdi Hajikhani <mhaji007@fiu.edu>
2021-05-11 11:57:59 -0400

    Set up registration form

--- src/components/accountBox/registerForm.js
+++ src/components/accountBox/registerForm.js
@@ -10,25 +10,22 @@ import {
 import { Marginer } from "../marginer";
 // import { AccountContext } from "./accountContext";
 
-export function LoginForm(props) {
+export function RegisterForm(props) {
   // const { switchToSignup } = useContext(AccountContext);
 
   return (
     <BoxContainer>
       <FormContainer>
-        <Input type="email" placeholder="Email"  />
+        <Input type="email" placeholder="Email" />
         <Input type="password" placeholder="Password" />
       </FormContainer>
       <Marginer direction="vertical" margin={10} />
       <MutedLink href="#">Forgot your password?</MutedLink>
       <Marginer direction="vertical" margin="1.6em" />
-      <SubmitButton type="submit">Login</SubmitButton>
+      <SubmitButton type="submit">Register</SubmitButton>
       <Marginer direction="vertical" margin="1em" />
       <MutedLink href="#">
-        Don't have an account?{" "}
-        <BoldLink href="#">
-          Register
-        </BoldLink>
+        Already have an account? <BoldLink href="#">Login</BoldLink>
       </MutedLink>
     </BoxContainer>
   );
